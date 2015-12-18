05a18775679e8ca6c3743619963d12ad19c329e4
brdooms <unknown>
2015-12-18 10:50:54 +0100

    verbose rsync errors

--- inc/backup.class.inc.php
+++ inc/backup.class.inc.php
@@ -2,24 +2,22 @@
 
 class Backup
 {
+
     public $App;
-      
     public $settings;
-    
     private $ssh;
-    
-            
+
     function __construct($App)
     {
         $this->App = $App;
-        
+
         $this->settings = $this->App->settings;
-        
-        $this->ssh = "ssh ".$this->settings['remote']['user']."@".$this->settings['remote']['host'];
-        
+
+        $this->ssh = "ssh " . $this->settings['remote']['user'] . "@" . $this->settings['remote']['host'];
+
         $this->rsyncdir = $this->settings['local']['rsyncdir'];
     }
-    
+
     function init()
     {
         //validate (check if LOCK file exists)
@@ -38,7 +36,40 @@ class Backup
         //rsync
         $this->rsync();
     }
-    
+
+    function get_rsync_status($rsync_code)
+    {
+        //list error codes
+        $codes = [];
+        $codes[0] = 'Success';
+        $codes[1] = 'Syntax or usage error';
+        $codes[2] = 'Protocol incompatibility';
+        $codes[3] = 'Errors selecting input/output files, dirs';
+        $codes[4] = 'Requested  action not supported: an attempt was made to manipulate 64-bit files on a platform that cannot support them; or an option was specified that is supported by the client and not by the server.';
+        $codes[5] = 'Error starting client-server protocol';
+        $codes[6] = 'Daemon unable to append to log-file';
+        $codes[10] = 'Error in socket I/O';
+        $codes[11] = 'Error in file I/O';
+        $codes[12] = 'Error in rsync protocol data stream';
+        $codes[13] = 'Errors with program diagnostics';
+        $codes[14] = 'Error in IPC code';
+        $codes[20] = 'Received SIGUSR1 or SIGINT';
+        $codes[21] = 'Some error returned by waitpid()';
+        $codes[22] = 'Error allocating core memory buffers';
+        $codes[23] = 'Partial transfer due to error';
+        $codes[24] = 'Partial transfer due to vanished source files';
+        $codes[25] = 'The --max-delete limit stopped deletions';
+        $codes[30] = 'Timeout in data send/receive';
+        $codes[35] = 'Timeout waiting for daemon connection';
+        //message
+        $message = '';
+        if (is_int($rsync_code) && isset($codes[$rsync_code]))
+        {
+            $message = $codes[$rsync_code];
+        }
+        return $message;
+    }
+
     function mysql()
     {
         #####################################
@@ -47,14 +78,14 @@ class Backup
         $this->App->out('Mysql backups', 'header');
         //check config directories 
         $dirs = [];
-        if(isset($this->settings['mysql']['configdirs']) && !empty($this->settings['mysql']['configdirs']))
+        if (isset($this->settings['mysql']['configdirs']) && !empty($this->settings['mysql']['configdirs']))
         {
-             $dirs = explode(',', $this->settings['mysql']['configdirs']);
+            $dirs = explode(',', $this->settings['mysql']['configdirs']);
         }
         //assume home dir
         else
         {
-            $dirs []= '~';
+            $dirs [] = '~';
         }
         //cache config files
         $cached = [];
@@ -64,9 +95,9 @@ class Backup
             $output = false;
             //check if allowed
             $this->App->Cmd->exe("$this->ssh 'cd $dir';");
-            if($this->App->Cmd->is_error())
+            if ($this->App->Cmd->is_error())
             {
-                $this->App->warn('Cannot access remote dir ' . $dir.'...');
+                $this->App->warn('Cannot access remote dir ' . $dir . '...');
             }
             else
             {
@@ -80,7 +111,7 @@ class Backup
             else
             {
                 $configfiles = [];
-                $this->App->warn('Cannot find mysql config files in remote dir ' . $dir.'...');
+                $this->App->warn('Cannot find mysql config files in remote dir ' . $dir . '...');
                 continue;
             }
             if (count($configfiles))
@@ -147,11 +178,11 @@ class Backup
             $this->App->out('Remote script configured, validating...');
             $script = $this->settings['remote']['pre-backup-script'];
             //test if the script exists
-            $this->App->Cmd->exe($this->ssh ." 'test -x ".$script."'");
-            if($this->App->Cmd->is_error())
+            $this->App->Cmd->exe($this->ssh . " 'test -x " . $script . "'");
+            if ($this->App->Cmd->is_error())
             {
                 $message = 'Remote script is not an executable script!';
-                if($this->settings['remote']['pre-backup-onfail'] == 'abort')
+                if ($this->settings['remote']['pre-backup-onfail'] == 'abort')
                 {
                     $this->App->fail($message);
                 }
@@ -162,11 +193,11 @@ class Backup
             }
             //run remote command
             $this->App->out('Running remote script...');
-            $output = $this->App->Cmd->exe($this->ssh ." '".$script."'");
-            if($this->App->Cmd->is_error())
+            $output = $this->App->Cmd->exe($this->ssh . " '" . $script . "'");
+            if ($this->App->Cmd->is_error())
             {
                 $message = 'Remote script did not run successfully!';
-                if($this->settings['remote']['pre-backup-onfail'] == 'abort')
+                if ($this->settings['remote']['pre-backup-onfail'] == 'abort')
                 {
                     $this->App->fail($message);
                 }
@@ -191,7 +222,7 @@ class Backup
     function meta()
     {
         //variables
-        $filebase = strtolower($this->settings['local']['hostdir-name'] . '.'.$this->App->settings['application']['name']);
+        $filebase = strtolower($this->settings['local']['hostdir-name'] . '.' . $this->App->settings['application']['name']);
         $this->App->out('Remote meta data', 'header');
         //disk layout
         if ($this->settings['meta']['remote-disk-layout'])
@@ -246,7 +277,7 @@ class Backup
                             $this->App->fail('Failed to retrieve package list!');
                         }
                     }
-                    break; 
+                    break;
                 case 'Arch':
                 case 'Manjaro':
                     $this->App->Cmd->exe("$this->ssh \"pacman -Qet\" > $this->rsyncdir/meta/" . $filebase . ".packages.txt");
@@ -257,7 +288,7 @@ class Backup
             }
         }
     }
-    
+
     function prepare()
     {
         #####################################
@@ -271,21 +302,21 @@ class Backup
         #####################################
         # OTHER DIRS
         #####################################
-        $a = ['meta', 'files']; 
+        $a = ['meta', 'files'];
         if ($this->settings['mysql']['enabled'])
         {
-            $a []= 'mysql';
+            $a [] = 'mysql';
         }
-        foreach($a as $aa)
+        foreach ($a as $aa)
         {
-            if (!file_exists($this->rsyncdir.'/'.$aa))
+            if (!file_exists($this->rsyncdir . '/' . $aa))
             {
                 $this->App->out("Create $aa dir $this->rsyncdir/$aa...");
                 $this->App->Cmd->exe("mkdir -p $this->rsyncdir/$aa");
             }
         }
     }
-    
+
     function rsync()
     {
         //rsync backups
@@ -299,8 +330,8 @@ class Backup
 
         //ssh
         $ssh = $this->App->Cmd->parse('{SSH}');
-        $o []= '-e "'.$ssh.' -o TCPKeepAlive=yes -o ServerAliveInterval=30"';
-        
+        $o [] = '-e "' . $ssh . ' -o TCPKeepAlive=yes -o ServerAliveInterval=30"';
+
         # general options
         if ($this->settings['rsync']['verbose'])
         {
@@ -326,25 +357,25 @@ class Backup
         foreach ($this->settings['included'] as $source => $target)
         {
             //check trailing slash
-            $sourcedir = (preg_match('/\/$/', $source))? $source:"$source/";
+            $sourcedir = (preg_match('/\/$/', $source)) ? $source : "$source/";
             $targetdir = "$this->rsyncdir/files/$target/";
-            
+
             //exclude dirs
             $excluded = [];
-            if(isset($this->settings['excluded'][$source]))
+            if (isset($this->settings['excluded'][$source]))
             {
                 $exludedirs = explode(',', $this->settings['excluded'][$source]);
-                    
+
                 foreach ($exludedirs as $d)
                 {
-                    $excluded []= "--exclude=$d";
+                    $excluded [] = "--exclude=$d";
                 }
             }
             //excluded files
             $excluded = implode(' ', $excluded);
             //output command
-            $this->App->out("rsync '$source' @ ".date('Y-m-d H:i:s')."...", 'indent');
-            if(!is_dir("$this->rsyncdir/files/$target"))
+            $this->App->out("rsync '$source' @ " . date('Y-m-d H:i:s') . "...", 'indent');
+            if (!is_dir("$this->rsyncdir/files/$target"))
             {
                 $this->App->out("Create target dir $this->rsyncdir/files/$target...");
                 $this->App->Cmd->exe("mkdir -p $this->rsyncdir/files/$target");
@@ -354,15 +385,15 @@ class Backup
             //obviously try rsync at least once :)
             $attempts = 1;
             //retry attempts on rsync fail
-            if(isset($this->settings['rsync']['retry-count']))
+            if (isset($this->settings['rsync']['retry-count']))
             {
-                $attempts += (integer)$this->settings['rsync']['retry-count'];
+                $attempts += (integer) $this->settings['rsync']['retry-count'];
             }
             //retry timeout between attempts
             $timeout = 0;
-            if(isset($this->settings['rsync']['retry-timeout']))
+            if (isset($this->settings['rsync']['retry-timeout']))
             {
-                $timeout += (integer)$this->settings['rsync']['retry-timeout'];
+                $timeout += (integer) $this->settings['rsync']['retry-timeout'];
             }
             $i = 1;
             $success = false;
@@ -370,18 +401,23 @@ class Backup
             {
                 $output = $this->App->Cmd->exe("$cmd");
                 $this->App->out($output);
-                //allow 24
-                if ($this->App->Cmd->exit_status == 24)
+                //WARNINGS - allow some rsync errors to occur
+                if (in_array($this->App->Cmd->exit_status, [24]))
                 {
-                    $this->App->warn('Rsync of '.$sourcedir.' directory returned exit code 24...');
+                    $message = $this->get_rsync_status($this->App->Cmd->exit_status);
+                    $message = (empty($message)) ? '' : ': "' . $message . '".';
+                    $this->App->warn("Rsync of $sourcedir directory exited with a non-zero status! Non fatal, will continue. Exit status: " . $this->App->Cmd->exit_status . $message);
                     $success = true;
                     break;
                 }
+                //ERRORS
                 elseif ($this->App->Cmd->exit_status != 0)
                 {
-                    $this->App->warn("Rsync of $sourcedir directory attempt $i/$attempts failed! Exit code ".$this->App->Cmd->exit_status.'...');
+                    $message = $this->get_rsync_status($this->App->Cmd->exit_status);
+                    $message = (empty($message)) ? '' : ': "' . $message . '".';
+                    $this->App->warn("Rsync of $sourcedir directory attempt $i/$attempts exited with a non-zero status! Fatal, will abort. Exit status " . $this->App->Cmd->exit_status . $message);
                     $message = [];
-                    if($i != $attempts)
+                    if ($i != $attempts)
                     {
                         $message [] = "Will retry rsync attempt " . ($i + 1) . " of $attempts in $timeout second(s)...\n";
                         sleep($timeout);
@@ -389,6 +425,7 @@ class Backup
                     $this->App->out(implode(' ', $message));
                     $i++;
                 }
+                //SUCCESS
                 else
                 {
                     $this->App->out("");
@@ -399,31 +436,9 @@ class Backup
             //check if successful
             if (!$success)
             {
-                //list error codes
-                $codes = [];
-                $codes[0] = 'Success';
-                $codes[1] = 'Syntax or usage error';
-                $codes[2] = 'Protocol incompatibility';
-                $codes[3] = 'Errors selecting input/output files, dirs';
-                $codes[4] = 'Requested  action not supported: an attempt was made to manipulate 64-bit files on a platform that cannot support them; or an option was specified that is supported by the client and not by the server.';
-                $codes[5] = 'Error starting client-server protocol';
-                $codes[6] = 'Daemon unable to append to log-file';
-                $codes[10] = 'Error in socket I/O';
-                $codes[11] = 'Error in file I/O';
-                $codes[12] = 'Error in rsync protocol data stream';
-                $codes[13] = 'Errors with program diagnostics';
-                $codes[14] = 'Error in IPC code';
-                $codes[20] = 'Received SIGUSR1 or SIGINT';
-                $codes[21] = 'Some error returned by waitpid()';
-                $codes[22] = 'Error allocating core memory buffers';
-                $codes[23] = 'Partial transfer due to error';
-                $codes[24] = 'Partial transfer due to vanished source files';
-                $codes[25] = 'The --max-delete limit stopped deletions';
-                $codes[30] = 'Timeout in data send/receive';
-                $codes[35] = 'Timeout waiting for daemon connection';
-                $message = $codes[$this->App->Cmd->exit_status];
-                $message = (empty($message))? '':': "'.$message.'"';
-                $this->App->fail("Rsync $sourcedir failed! Exit status ".$this->App->Cmd->exit_status.$message);
+                $message = $this->get_rsync_status($this->App->Cmd->exit_status);
+                $message = (empty($message)) ? '' : ': "' . $message . '".';
+                $this->App->fail("Rsync of $sourcedir directory failed! Aborting! Exit status " . $this->App->Cmd->exit_status . $message);
             }
         }
         $this->App->out("Done!");
@@ -435,39 +450,46 @@ class Backup
         # CREATE LOCK FILE
         #####################################
         # check for lock
-        if (file_exists( $this->settings['local']['hostdir'] . "/LOCK"))
+        if (file_exists($this->settings['local']['hostdir'] . "/LOCK"))
         {
-            $this->App->fail("LOCK file ".$this->settings['local']['hostdir']."/LOCK exists!", 'LOCKED');
+            $this->App->fail("LOCK file " . $this->settings['local']['hostdir'] . "/LOCK exists!", 'LOCKED');
         }
         else
         {
             $this->App->out('Create LOCK file...');
-            $this->App->Cmd->exe("touch " .  $this->settings['local']['hostdir'] . "/LOCK");
+            $this->App->Cmd->exe("touch " . $this->settings['local']['hostdir'] . "/LOCK");
         }
     }
+
 }
 
 class BTRFSBackup extends Backup
 {
+
     function create_syncdir()
     {
-        $this->App->Cmd->exe("btrfs subvolume create " .  $this->rsyncdir);
+        $this->App->Cmd->exe("btrfs subvolume create " . $this->rsyncdir);
     }
+
 }
 
 class DefaultBackup extends Backup
 {
+
     function create_syncdir()
     {
-        $this->App->Cmd->exe("mkdir -p " .  $this->rsyncdir);
+        $this->App->Cmd->exe("mkdir -p " . $this->rsyncdir);
     }
+
 }
 
 class ZFSBackup extends Backup
 {
+
     function create_syncdir()
     {
         $rsyncdir = preg_replace('/^\//', '', $this->rsyncdir);
-        $this->App->Cmd->exe("zfs create " .  $rsyncdir);
+        $this->App->Cmd->exe("zfs create " . $rsyncdir);
     }
+
 }
